c9289724c5128e0d146866d82ea5e88615ddc61c
Tobias Tykvart <tobias.tykvart@posteo.de>
2021-04-13 13:44:52 +0200

    Change example files, add dependencies
    The goal is to make this the React template repository so I added a Hello World example and test and removed the other example files. I also added dependencies for icon collections and to make routing between multiple pages possible.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CheckboxWithLabel from './components/checkboxWithLabel/checkboxWithLabel';
-import Link from './components/link/link';
+import HelloWorld from './components/hellloWorld/HelloWorld';
 
-const App = () => (
-  <h1>My React and TypeScript App!! {new Date().toLocaleDateString()}</h1>
-);
+const App = () => <HelloWorld />;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <CheckboxWithLabel labelOn="On" labelOff="Off" />
-    <br />
-    <Link page="http://www.facebook.com">Facebook</Link>
   </React.StrictMode>,
   document.getElementById('root')
 );
