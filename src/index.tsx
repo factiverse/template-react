84d8922200496af39bb3a51e7675119cb78b0d49
Tobias Tykvart <tobiastykvart@Tobiass-MacBook-Pro-16.local>
2023-03-31 16:37:37 +0200

    Install Auth0, update to React18 rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './components/App/App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container as Element);
+
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="RouterTest" element={<div>This is a routed page</div>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Auth0Provider
+      domain="dev-on-lc56a.us.auth0.com"
+      clientId="lCEQ7MOgmWvFNYgwRc96lSzRaPguw0Yc"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="RouterTest" element={<div>This is a routed page</div>} />
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
+  </React.StrictMode>
 );
